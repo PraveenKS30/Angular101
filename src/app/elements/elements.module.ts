1d5ee9fbbf8cbb0dfa7afe08341d7847e84f5b57
PraveenKS30 <c2praveen30jun@gmail.com>
2021-09-19 18:59:06 +0530

    sample-module initial commit

---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+
+
+@NgModule({
+  declarations: [
+    ElementsHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    ElementsRoutingModule
+  ],
+  //added to export elements-home-component
+  exports: [
+    ElementsHomeComponent
+  ]
+})
+export class ElementsModule { }
