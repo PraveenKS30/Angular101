4e36624ec98c14a0d2ba2de3db1f4604e318022e
PraveenKS30 <c2praveen30jun@gmail.com>
2021-09-22 10:08:17 +0530

    initial sample-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionsModule } from './collections/collections.module';
+import { ElementsModule } from './elements/elements.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatTabsModule,
+    //changing order so that Element and Collection routing module have preference
+    //over app routing module
+    //this would make sure ** don't throw not found for Element and collection as well
+    ElementsModule,
+    CollectionsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
