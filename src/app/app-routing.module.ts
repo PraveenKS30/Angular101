d27178862b04375d330ed7b2d605740ba3f3e5b8
PraveenKS30 <c2praveen30jun@gmail.com>
2021-09-26 23:05:11 +0530

    sample lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'elements', 
+    loadChildren: ()=> import('./elements/elements.module').then(m => m.ElementsModule) 
+  },
+  {
+    path: 'collections',
+    loadChildren :() => import('./collections/collections.module').then(m=>m.CollectionsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
