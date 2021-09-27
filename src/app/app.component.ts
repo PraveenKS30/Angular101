29ca4598416486d39056b990516474b033100735
PraveenKS30 <c2praveen30jun@gmail.com>
2021-09-27 12:55:03 +0530

    sample-services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sample-services';
+ 
+  constructor(private wikipedia : WikipediaService){}
+
+  onSearchResponse(){
+    this.wikipedia.search().subscribe(response => {
+      console.log(response)
+    })
+  }
 }
